82c0b3b741e57a01e25ca269b08a39249bcdd179
etienne-napoleone <etienne@tomochain.com>
2018-06-21 14:16:04 +0700

    Updated entrypoint and Dockerfile

--- tomochain-node/entrypoint.sh
+++ tomochain-node/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-set -x
-
 # constants
 DATA_DIR="data"
 KEYSTORE_DIR="keystore"
+GENESIS_FILE="genesis.json"
+GENESIS_PATH="genesis/$GENESIS_FILE"
 BOOTNODES_FILE="bootnodes"
 BOOTNODES_PATH="bootnodes/$BOOTNODES_FILE"
-PASSWORD_FILE="MASTERNODE_WALLET_PASSWORD"
+PASSWORD_FILE="masternode_wallet_password"
 PASSWORD_PATH="/run/secrets/$PASSWORD_FILE"
 
 # vars from docker env
@@ -15,19 +15,12 @@ PASSWORD_PATH="/run/secrets/$PASSWORD_FILE"
 # - IDENTITY (default to unnamed_node)
 # - IS_BOOTNODE (default to false)
 
-# set tomo default params
-params="--datadir $DATA_DIR "
-params="$params --keystore $KEYSTORE_DIR"
-params="$params --identity $IDENTITY"
-params="$params --networkid 89"
-params="$params --rpc"
-params="$params --rpccorsdomain '*'"
-params="$params --rpcaddr 0.0.0.0"
-params="$params --rpcport 8545"
-params="$params --rpcvhosts '*'"
-params="$params --mine"
-params="$params --gasprice '1'"
-params="$params --targetgaslimit '420000000'"
+params=""
+
+# if no blockchain data, init the genesis block
+if [[ ! -d $DATA_DIR/tomo ]]; then
+  tomo init $GENESIS_PATH --datadir $DATA_DIR 2> /dev/null
+fi
 
 # set tomo password param
 if [[ -f $PASSWORD_PATH ]]; then
@@ -39,24 +32,47 @@ fi
 
 # set tomo bootnode param (or dump enode in bootnode file if IS_BOOTNODE)
 if [[ "$IS_BOOTNODE" = true ]]; then
+  echo "Dumping self enode address to $BOOTNODES_PATH"
   tomo js getenode.js --datadir $DATA_DIR --keystore $KEYSTORE_DIR \
+  2> /dev/null \
   | sed "s/::/$(hostname -i)/g"
   > $BOOTNODES_PATH
 else
+  echo "Adding bootnodes to startup params"
   sleep 10
   params="$params --bootnodes $BOOTNODES_PATH"
 fi
 
 # set tomo unlock param (the account to use)
 accounts=$(tomo account list --datadir $DATA_DIR  --keystore $KEYSTORE_DIR)
-if [[ $INDEX -le $(echo $accounts | wc -l) ]]; then
+echo "Accounts:"
+echo $accounts
+if [[ $INDEX -le $(echo "$accounts" | wc -l) && $INDEX -ge 1 ]]; then
   account=$(
     tomo account list --datadir $DATA_DIR  --keystore $KEYSTORE_DIR \
+    2> /dev/null \
     | head -n $INDEX \
     | tail -n 1 \
     | cut -d"{" -f 2 | cut -d"}" -f 1
   )
+  echo "Using account $account"
+  params="$params --unlock $account"
+else
+  echo "Your index [$INDEX] does not match the number of accounts [$(echo "$accounts" | wc -l)] . Exiting..."
+  exit
 fi
 
-
-tomo $params
+set -x
+tomo $params \
+     --datadir $DATA_DIR \
+     --keystore $KEYSTORE_DIR \
+     --identity $IDENTITY \
+     --networkid 89 \
+     --rpc \
+     --rpccorsdomain "*" \
+     --rpcaddr 0.0.0.0 \
+     --rpcport 8545 \
+     --rpcvhosts "*" \
+     --mine \
+     --gasprice "1" \
+     --targetgaslimit "420000000"
