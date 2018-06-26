b18388b35d2a1691821229fd8d2382fd78b00b86
etienne-napoleone <etienne@tomochain.com>
2018-06-26 09:51:44 +0700

    Add init script for first run of the infra Crreate the masternodes keystore

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 for i in {1..3}; do
-  docker run -v $(pwd)/password:/tomochain/password tomochain/tomochain:latest account new --password ./password --keystore ./tomochain/keystore
+  docker run \
+    -v $(pwd)/deploy/ressources/password:/tomochain/password \
+    -v $(pwd)/deploy/ressources/keystore/:/tomochain/keystore/ \
+    tomochain/tomochain:latest \
+    account new --password ./password --keystore ./keystore
 done
-
-docker run
