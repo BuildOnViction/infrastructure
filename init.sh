9faae5f56237404df3c35b07872272cd8361bf92
etienne-napoleone <etienne@tomochain.com>
2018-06-29 17:55:18 +0700

    Add genesis.json and remove init_script Not needed anymore as they are all static files

--- init.sh
+++ init.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 set -ex
 
-docker volume create keystore
-docker volume create genesis
-docker run --rm \
-  -v $(pwd)/deploy/ressources/password:/build/password \
-  -v $(pwd)/deploy/init/init_script.sh:/build/init_script.sh \
-  -v $(pwd)/deploy/init/template.json:/build/template.json \
-  -v keystore:/build/keystore \
-  -v genesis:/build/genesis \
-  --entrypoint=/build/init_script.sh \
-  tomochain/tomochain:latest
 docker-compose \
   -f deploy/docker-compose.masternodes.yml \
   -f deploy/docker-compose.metrics.yml \
