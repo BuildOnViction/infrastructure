4ac01de46bee8365bdfed4bcaddd7c28c93728ac
etienne-napoleone <etienne@tomochain.com>
2018-07-02 17:28:46 +0700

    Update tomomaster conf Now version the compiled smartcontracts with the right network address. To be automated soon.

---
+++ tomomaster/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+MNEMONIC_FILE="mnemonic_tomomaster"
+MNEMONIC_PATH="/run/secrets/$PASSWORD_FILE"
+
+sed -i -e "s/:mnemonic:/$(cat $MNEMONIC_PATH)/g" local.json
+echo $(cat $MNEMONIC_PATH)
+
+mv local.json config
+npm build
+mv TomoValidator.json build/contracts
+mv TomoRandomize.json build/contracts
+mv BlockSigner.json build/contracts
+
+npm run dev
