aa2aa4e8bc9574048e505dcf677369224320c0d7
etienne-napoleone <etienne@tomochain.com>
2018-07-03 11:06:32 +0700

    Add undeploy and build script

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-set -x
-ddd
+set -ex
+
 docker-compose \
   -f deploy/docker-compose.masternodes.yml \
   -f deploy/docker-compose.metrics.yml \
   -f deploy/docker-compose.netstats.yml \
   -f deploy/docker-compose.tomomaster.yml \
   -f deploy/docker-compose.tomoscan.yml \
-  down
-docker volume prune
+  build
