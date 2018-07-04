58408b86b01972968a3adf08ac4aad66eb87518d
etienne-napoleone <etienne@tomochain.com>
2018-07-04 16:59:26 +0700

    Fix build of smart contracts for tomomaster

--- tomomaster/entrypoint.sh
+++ tomomaster/entrypoint.sh
@@ -6,9 +6,13 @@ sed -i -e "s/:mnemonic:/$(cat $MNEMONIC_PATH)/g" local.json
 echo $(cat $MNEMONIC_PATH)
 
 mv local.json config
+
+truffle migrate --compile-all --reset development
+
+mv TomoValidator.json build/contracts/TomoValidator.json
+mv TomoRandomize.json build/contracts/TomoRandomize.json
+mv BlockSigner.json build/contracts/BlockSigner.json
+
 npm run build
-mv TomoValidator.json build/contracts
-mv TomoRandomize.json build/contracts
-mv BlockSigner.json build/contracts
 
 npm run dev
