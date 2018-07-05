68abe2a74f28c978948dddf6dc7b1a265af77d8e
etienne-napoleone <etienne@tomochain.com>
2018-07-05 14:32:30 +0700

    Use .env file instead of complicated script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-set -ex
 
-docker-compose -p tomochain \
-  -f deploy/docker-compose.tomochain.yml \
-  -f deploy/docker-compose.metrics.yml \
-  -f deploy/docker-compose.netstats.yml \
-  -f deploy/docker-compose.tomomaster.yml \
-  -f deploy/docker-compose.tomoscan.yml \
-up -d --build
+docker-compose up -d --build
--- undeploy.sh
+++ undeploy.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-set -x
 
-docker-compose -p tomochain \
-  -f deploy/docker-compose.tomochain.yml \
-  -f deploy/docker-compose.metrics.yml \
-  -f deploy/docker-compose.netstats.yml \
-  -f deploy/docker-compose.tomomaster.yml \
-  -f deploy/docker-compose.tomoscan.yml \
-down
+docker-compose down
 docker volume prune
