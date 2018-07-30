a8a9baf629bcafe69c5a9448c232d5a536b8c3a3
etienne-napoleone <etienne@tomochain.com>
2018-07-30 17:13:51 +0700

    We don't want auto update here

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for file in nodes netstats metrics tomomaster tomoscan proxy cd; do
+for file in nodes netstats metrics tomomaster tomoscan proxy; do
   docker stack deploy -c deploy/${file}.yml devnet
 done
