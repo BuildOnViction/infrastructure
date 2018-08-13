8f7b893ef901520e5fb33faa1d8ed0d626d3413c
etienne-napoleone <etienne@tomochain.com>
2018-08-13 19:19:06 +0700

    Add IP parameter to bootnode

--- bootnode/entrypoint.sh
+++ bootnode/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -x
 
+params=""
+
 # file to env
-for env in PRIVATE_KEY; do
+for env in PRIVATE_KEY IP; do
   file=$(eval echo "\$${env}_FILE")
   if [[ -f $file ]] && [[ ! -z $file ]]; then
     echo "Replacing $env by $file"
@@ -9,17 +11,17 @@ for env in PRIVATE_KEY; do
   fi
 done
 
+
+# private key
+if [[ ! -z $IP ]]; then
+  params="$params -nat extip:$IP"
+fi
+
 # private key
 if [[ -z $PRIVATE_KEY ]]; then
-  bootnode -genkey bootnode.key
+  bootnode -genkey bootnode.key $params
 else
   echo "$PRIVATE_KEY" > bootnode.key
 fi
 
-# dump address
-address="enode://$(bootnode -nodekey bootnode.key -writeaddress)@[$(hostname -i)]:30301"
-
-echo "Bootnode address is $address"
-echo "$address" > ./bootnodes/bootnodes
-
-exec bootnode -nodekey bootnode.key
+exec bootnode -nodekey bootnode.key $params
