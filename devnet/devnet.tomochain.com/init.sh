53ae9f843df56cf334700dfa4a4b765e6afe72b2
etienne-napoleone <etienne@tomochain.com>
2018-08-16 10:47:04 +0700

    refactor: change back to one swarm

--- devnet/devnet.tomochain.com/init.sh
+++ devnet/devnet.tomochain.com/init.sh
@@ -18,6 +18,27 @@ echo "New Netstats websocket secret ('WS_SECRET'): "
 unset input && read -s input \
 ; echo $input | docker secret create netstats_ws_secret -
 
+echo -e "\n[ tomochain ]\n"
+
+echo "New account password for node 'sun': "
+unset input && read -s input \
+; echo $input | docker secret create tomochain_password_sun -
+echo "Existing private key for node 'sun': "
+unset input && read -s input \
+; echo $input | docker secret create tomochain_pk_sun -
+echo "New account password for node 'moon': "
+unset input && read -s input \
+; echo $input | docker secret create tomochain_password_moon -
+echo "Existing private key for node 'moon': "
+unset input && read -s input \
+; echo $input | docker secret create tomochain_pk_moon -
+echo "New account password for node 'earth': "
+unset input && read -s input \
+; echo $input | docker secret create tomochain_password_earth -
+echo "Existing private key for node 'earth': "
+unset input && read -s input \
+; echo $input | docker secret create tomochain_pk_earth -
+
 echo -e "\n[ proxy ]\n"
 
 echo "Certificate pem file (path): "
