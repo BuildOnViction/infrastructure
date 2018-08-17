2a5349257831c04d7e9de10eb1c9669c233468c4
etienne-napoleone <etienne@tomochain.com>
2018-08-17 11:47:09 +0700

    revert to default tomo port

--- tomochain/entrypoint.sh
+++ tomochain/entrypoint.sh
@@ -116,7 +116,7 @@ exec tomo $params \
   --identity $IDENTITY \
   --password ./password \
   --networkid 89 \
-  --port 39393 \
+  --port 30303 \
   --rpc \
   --rpccorsdomain "*" \
   --rpcaddr 0.0.0.0 \
