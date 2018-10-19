12a8457c3279c3759ea50c3a45eb3d477edf9739
etienne-napoleone <etienne@tomochain.com>
2018-10-19 17:54:21 +0700

    feat(docker): add graylog image

---
+++ docker/graylog/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [[ ! -z $GRAYLOG_PASSWORD_SECRET_FILE ]]; then
+  export GRAYLOG_PASSWORD_SECRET=$(cat $GRAYLOG_PASSWORD_SECRET_FILE)
+fi
+
+if [[ ! -z $GRAYLOG_ROOT_PASSWORD_SHA2_FILE ]]; then
+  export GRAYLOG_ROOT_PASSWORD_SHA2=$(cat $GRAYLOG_ROOT_PASSWORD_SHA2_FILE)
+fi
+
+exec /docker-entrypoint.sh "$@"
