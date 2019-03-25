9f5a8b6b48baedd4690694621503a9adf7d6fea1
etienne-napoleone <etienne@tomochain.com>
2019-03-25 10:33:06 +0700

    fix: correct output

---
+++ terraform/devnet/output.tf
@@ -0,0 +1,6 @@
+output "tomoscan_ip" {
+  value = "${module.tomoscan.loadbalancer_ip}"
+}
+output "tomomaster_ip" {
+  value = "${module.tomomaster.loadbalancer_ip}"
+}
--- terraform/modules/tomoscan/outputs.tf
+++ terraform/modules/tomoscan/outputs.tf
@@ -1,3 +1,3 @@
 output "loadbalancer_ip" {
-  value = kubernetes_service.scan.load_balancer_ip
+  value = "${kubernetes_service.scan.load_balancer_ingress.0.ip}"
 }
