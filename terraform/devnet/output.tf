e980f4a26981b006b03aa78f5a0c2b2bb07b2b72
Etienne Napoleone <etienne@tomochain.com>
2019-05-07 15:22:57 +0700

    fix: comment output for tomomaster

--- terraform/devnet/output.tf
+++ terraform/devnet/output.tf
@@ -2,9 +2,9 @@ output "tomoscan_ip" {
   value = "${module.tomoscan.loadbalancer_ip}"
 }
 
-output "tomomaster_ip" {
-  value = "${module.tomomaster.loadbalancer_ip}"
-}
+# output "tomomaster_ip" {
+#   value = "${module.tomomaster.loadbalancer_ip}"
+# }
 
 output "netdata_ip" {
   value = "${module.netdata.loadbalancer_ip}"
