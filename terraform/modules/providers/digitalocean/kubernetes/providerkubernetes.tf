9631e3e459142b98d365247bb876552517345b96
Etienne Napoleone <etienne@tomochain.com>
2019-05-13 18:05:26 +0700

    feat: remove providers from module

--- terraform/modules/providers/digitalocean/kubernetes/providerkubernetes.tf
+++
@@ -1,7 +0,0 @@
-provider "kubernetes" {
-  host = "${digitalocean_kubernetes_cluster.cluster.endpoint}"
-
-  client_certificate     = "${base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.client_certificate)}"
-  client_key             = "${base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.client_key)}"
-  cluster_ca_certificate = "${base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)}"
-}
