e7b850e59796b929575b61aa2f270d32a62cd954
Etienne Napoleone <etienne@tomochain.com>
2019-05-13 18:22:47 +0700

    feat: use new do k8s module for tf devnet

--- terraform/devnet/main.tf
+++ terraform/devnet/main.tf
@@ -3,11 +3,27 @@ provider "digitalocean" {
 }
 
 provider "kubernetes" {
-  host = "${digitalocean_kubernetes_cluster.devnet.endpoint}"
+  host = "${module.kubernetes.endpoint}"
 
-  client_certificate     = "${base64decode(digitalocean_kubernetes_cluster.devnet.kube_config.0.client_certificate)}"
-  client_key             = "${base64decode(digitalocean_kubernetes_cluster.devnet.kube_config.0.client_key)}"
-  cluster_ca_certificate = "${base64decode(digitalocean_kubernetes_cluster.devnet.kube_config.0.cluster_ca_certificate)}"
+  client_certificate     = "${module.kubernetes.client_certificate}"
+  client_key             = "${module.kubernetes.client_key}"
+  cluster_ca_certificate = "${module.kubernetes.cluster_ca_certificate}"
+}
+
+provider "helm" {
+    kubernetes {
+      host     = "${module.kubernetes.endpoint}"
+
+      client_certificate     = "${module.kubernetes.client_certificate}"
+      client_key             = "${module.kubernetes.client_key}"
+      cluster_ca_certificate = "${module.kubernetes.cluster_ca_certificate}"
+    }
+}
+
+module "kubernetes" {
+  source = "../modules/providers/digitalocean/kubernetes"
+  
+  name = "devnet"
 }
 
 # module "tomomaster" {
