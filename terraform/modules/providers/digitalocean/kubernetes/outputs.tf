3fe2d9dff7d048d18e4dc949f9155bf23cb2ce69
Etienne Napoleone <etienne@tomochain.com>
2019-05-13 18:22:13 +0700

    feat: add outputs for do k8s module

---
+++ terraform/modules/providers/digitalocean/kubernetes/outputs.tf
@@ -0,0 +1,12 @@
+output "endpoint" {
+  value = "${digitalocean_kubernetes_cluster.cluster.endpoint}"
+}
+output "client_certificate" {
+  value = "${base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.client_certificate)}"
+}
+output "client_key" {
+  value = "${base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.client_key)}"
+}
+output "cluster_ca_certificate" {
+  value = "${base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)}"
+}
